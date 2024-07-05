ba656d946a73ba7d88864c1be8ae5bc8cff8248e
ashu <ashu@ashus-MacBook-Pro.local>
2024-07-05 14:36:29 -0700

    backend to k8

--- src/app/poll.service.ts
+++ src/app/poll.service.ts
@@ -9,7 +9,7 @@ export class PollService {
 
   public _screenID?: string;
   constructor(private http: HttpClient) { }
-  host = 'http://167.99.24.118:80';
+  host = 'http://167.99.24.118:3000';
   // host = 'http://localhost:3000';
   sendTrace(screenID:string, actions: { x: number, y: number }[], username: string): void {
     // Send the coordinates to the server
